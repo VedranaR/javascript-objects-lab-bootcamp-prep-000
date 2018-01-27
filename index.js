51ae608a73ec0cb80f65dda8ea0b3e5c9f8a0150
Vedrana <vedrana_ravlic16@yahoo.com>
2018-01-27 21:55:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var recipes = {};
+
+
+function updateObjectWithKeyAndValue(object, key, value){
+	var obj = {prop:1};
+	obj[key] = value;
+	return obj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+	var obj = {prop:1};
+	obj[key] = value;
+	return obj;
+}
